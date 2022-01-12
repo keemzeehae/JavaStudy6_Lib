31463e3a0ed7cba4a2876ddb553a13dfb2783b80
keemzeehae <keemzeehae@naver.com>
2022-01-12 17:24:24 +0900

    member

--- src/com/jihye/s3/ex1/MemberData.java
+++ src/com/jihye/s3/ex1/MemberData.java
@@ -1,6 +1,7 @@
 package com.jihye.s3.ex1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import com.jihye.s2.util.token.SeasonDTO;
@@ -8,14 +9,58 @@ import com.jihye.s2.util.token.SeasonDTO;
 public class MemberData {
 	
 	private String data;
-	
+	private Scanner sc;
 	//기본 생성자 선언
 	public MemberData() {
+		this.sc = new Scanner(System.in);
 		this.data = "id1-pw1-name1-id1@gmail.com-12-";
 		this.data = this.data+"id2-pw2-name2-name2@naver.com-42-";
 		this.data = this.data+"id3-pw3-name3-name3@daum.net-19-";
 		this.data = this.data+"id4-pw4-name4-name4@daum.net-17-";
 		System.out.println(this.data);
+	}
+	public MemberDTO removeMember(ArrayList<MemberDTO> ar) {
+		//삭제하고 싶은 ID 입력받아서
+		//ArrayList에서 삭제
+		MemberDTO memberDTO = null;
+		System.out.println("아이디를 입력해주세요");
+		String id = sc.next();
+		System.out.println(id);
+		//int index = -1;
+		for (int i=0;i<ar.size();i++) {
+			System.out.println(ar.get(i).getId());
+			if(ar.get(i).getId().equals(id)) {
+				//index= i;
+				memberDTO=ar.remove(i);		//Arraylist 에서 삭제
+				break;
+			}
+			
+		}
+	
+		return memberDTO;
+
+	}
+	public void addMember(ArrayList<MemberDTO> ar) {
+		//새로추가할 MemberDTO 생성 
+		MemberDTO m1 = new MemberDTO();
+		System.out.println("id를 입력해주세요.");
+		m1.setId(sc.next());
+		System.out.println("pw를 입력해주세요.");
+		m1.setPw(sc.next());
+		System.out.println("이름을 입력해주세요.");
+		m1.setName(sc.next());
+		System.out.println("email을 입력해주세요.");
+		m1.setEmail(sc.next());
+		System.out.println("나이를 입력해주세요.");
+		m1.setAge(sc.nextInt());
+		
+		ar.add(m1);
+		
+
+		//키보드로부터 id, pw, name , email , age  입력 받아서 
+		//MemberDTO의 멤버변수 값으로 대입
+		//매개변수로 받은 ar에 MemberDTO 추가 
+		
 	}
 	//메서드
 	public ArrayList<MemberDTO> init() {
@@ -23,7 +68,21 @@ public class MemberData {
 	//MemberDTO를 생성해서 멤버변수값으로 대입
 	//MemberDTO들을 ArrayList에 담아서 리턴
 		System.out.println("Member Branch");
-		return null;
+		
+		StringTokenizer st = new StringTokenizer(this.data,"-");
+		ArrayList<MemberDTO> ar = new ArrayList<>();
+		
+		while(st.hasMoreTokens()) {
+			MemberDTO memberDTO = new MemberDTO();
+			memberDTO.setId(st.nextToken().trim());
+			memberDTO.setPw(st.nextToken().trim());
+			memberDTO.setName(st.nextToken().trim());
+			memberDTO.setEmail(st.nextToken().trim());
+			memberDTO.setAge(Integer.parseInt(st.nextToken().trim()));
+			ar.add(memberDTO);
+			
+		}
+		return ar;
 		
 	}
 }
--- src/com/jihye/s3/ex1/MemberMain.java
+++ src/com/jihye/s3/ex1/MemberMain.java
@@ -1,21 +1,31 @@
 package com.jihye.s3.ex1;
 
+import java.util.ArrayList;
+
 public class MemberMain {
 
 	public static void main(String[] args) {
 		// test하고 싶을때 프로그램을 실행하는 역할 main method
 		MemberData memberData = new MemberData();
-		MemberDTO [] members = memberData.init();
+		ArrayList<MemberDTO> members = memberData.init();
 		
-		for (int i =0; i<members.length;i++) {
-			System.out.println(members[i].getId());
-			System.out.println(members[i].getPw());
-			System.out.println(members[i].getName());
-			System.out.println(members[i].getEmail());
-			System.out.println(members[i].getAge());
+//		memberData.addMember(members);;
+		MemberDTO memberDTO= memberData.removeMember(members);
+		if (memberDTO !=null) {
+			System.out.println("삭제성공");
+			
+		}else {
+			System.out.println("삭제실패");
+		}
+		for (int i =0; i<members.size();i++) {
+			System.out.println(members.get(i).getId());
+			System.out.println(members.get(i).getPw());
+			System.out.println(members.get(i).getName());
+			System.out.println(members.get(i).getEmail());
+			System.out.println(members.get(i).getAge());
 			System.out.println("=========================");
 		}
-		System.out.println("name: "+"jh"+"age: "+30);
+		System.out.println("name: "+"jh"+"age: "+23);
 
 	}
 
