790c18ee919f955ad6f4ac3746fc5fb322a91ddc
keemzeehae <keemzeehae@naver.com>
2022-01-13 16:33:28 +0900

    commit ex1

--- src/com/jihye/s3/ex1/Ex1Main.java
+++ src/com/jihye/s3/ex1/Ex1Main.java
@@ -2,9 +2,13 @@ package com.jihye.s3.ex1;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import com.jihye.s3.ex1.member.MemberDTO;
 import com.jihye.s3.ex1.member.MemberData;
+import com.jihye.s3.ex1.student.StudentDTO;
 import com.jihye.s3.ex1.student.StudentData;
 
 public class Ex1Main {
@@ -15,8 +19,23 @@ public class Ex1Main {
 		
 		//회원들의 정보 모아놓음 
 		ArrayList<MemberDTO> ar= memberData.init();
-		studentData.addStudent(ar);
+		Map<String, StudentDTO> map =studentData.addStudent(ar);
+		Set<String> keys = map.keySet();
+		Iterator<String> k = keys.iterator();
 		
+		while(k.hasNext()) {
+			String key = k.next();
+			StudentDTO studentDTO = map.get(key);
+			System.out.println("Key: "+key);
+			System.out.println(studentDTO.getId());
+			System.out.println(studentDTO.getName());
+			System.out.println(studentDTO.getKor());
+			System.out.println(studentDTO.getEng());
+			System.out.println(studentDTO.getMath());
+			System.out.println(studentDTO.getTotal());
+			System.out.println(studentDTO.getAvg());
+		}
+		System.out.println(map);
 
 	}
 
--- src/com/jihye/s3/ex1/student/StudentData.java
+++ src/com/jihye/s3/ex1/student/StudentData.java
@@ -21,45 +21,35 @@ public class StudentData {
 		// 키보드로부터 member의 수만큼 성적 정보입력 ar 에서 출석부에서 사람이름 가지고 와서 student 성적 정보 입력해놓음
 		// 이 정보들을 StudentDTO
 		// map의 키는 id
-		MemberDTO memberDTO = new MemberDTO();
 
-		System.out.println("아이디를 입력해주세요");
-		String id = sc.next();
-		System.out.println(id);
-		HashMap<String, Integer> map = new HashMap<>();
+		HashMap<String, StudentDTO> map = new HashMap<>();
 
 		for (int i = 0; i < ar.size(); i++) {
 			StudentDTO studentDTO = new StudentDTO();
-			
-			System.out.println(ar.get(i).getId());
-			
-			if (ar.get(i).getId().equals(id)) {
-				System.out.println("정보있음");
-				ArrayList<StudentDTO> studentInfo = new ArrayList<>();
-				//id 
-				//name
-				System.out.println("국어성적을 입력하세요");
-				studentDTO.setKor(sc.nextInt());
-				System.out.println("영어성적을 입력하세요");
-				studentDTO.setEng(sc.nextInt());
-				System.out.println("수학성적을 입력하세요");
-				studentDTO.setMath(sc.nextInt());
-				//총점
-				//평균
-				studentDTO.setTotal(studentDTO.getKor()+studentDTO.getEng()+studentDTO.getMath());
-				studentDTO.setAvg(studentDTO.getTotal()/3.0);
-				System.out.println("총점 : "+studentDTO.getTotal());
-				System.out.println("평균 : "+studentDTO.getAvg());
-				studentInfo.add(studentDTO);
-				
-				//맵에 추가
-				
-				map.put(studentDTO.getId(), studentDTO);
-				
-				break;
-			}
+			studentDTO.setId(ar.get(i).getId());
+
+			// id
+			// name
+			studentDTO.setName(ar.get(i).getName());
+			System.out.println("국어성적을 입력하세요");
+			studentDTO.setKor(sc.nextInt());
+			System.out.println("영어성적을 입력하세요");
+			studentDTO.setEng(sc.nextInt());
+			System.out.println("수학성적을 입력하세요");
+			studentDTO.setMath(sc.nextInt());
+			// 총점
+			// 평균
+			studentDTO.setTotal(studentDTO.getKor() + studentDTO.getEng() + studentDTO.getMath());
+			studentDTO.setAvg(studentDTO.getTotal() / 3.0);
+			System.out.println("총점 : " + studentDTO.getTotal());
+			System.out.println("평균 : " + studentDTO.getAvg());
+
+			// 맵에 추가
+
+			map.put(studentDTO.getId(), studentDTO);
 
 		}
+
 		return map;
 
 	}
