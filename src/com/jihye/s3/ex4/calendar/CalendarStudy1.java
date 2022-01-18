d39702a500a065827185f374d5b835d5f6a05063
keemzeehae <keemzeehae@naver.com>
2022-01-18 21:45:44 +0900

    update calendar

--- src/com/jihye/s3/ex4/calendar/CalendarStudy1.java
+++ src/com/jihye/s3/ex4/calendar/CalendarStudy1.java
@@ -1,6 +1,7 @@
 package com.jihye.s3.ex4.calendar;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class CalendarStudy1 {
@@ -15,7 +16,35 @@ public class CalendarStudy1 {
 		
 		GregorianCalendar gc = new GregorianCalendar();
 		Calendar calendar = new GregorianCalendar();
-		Calendar ca =Calendar.getInstance();
+		Calendar ca =Calendar.getInstance();//실제로 오는 건 Gregorian이다
+		
+		System.out.println(gc);
+		System.out.println(calendar);
+		System.out.println(ca);
+		
+		int y = ca.get(Calendar.YEAR);
+		System.out.println("Year: "+y);
+		int m = ca.get(Calendar.MONTH);
+		System.out.println("Month: "+m);
+		int d = ca.get(Calendar.DATE);
+		System.out.println("Date: "+d);
+		int h = ca.get(Calendar.HOUR);
+		System.out.println("Hour: "+h);
+		int min = ca.get(Calendar.MINUTE);
+		System.out.println("Minute: "+min);
+		int sc = ca.get(Calendar.SECOND);
+		System.out.println("Second: "+sc);
+		int mil = ca.get(Calendar.MILLISECOND);
+		System.out.println("Milli: "+mil);
+		
+		
+		long millis = ca.getTimeInMillis();
+		System.out.println("Millis: "+millis);
+		
+		Date date = ca.getTime();
+		System.out.println(date);
+		//getTime을 쓰는 이유는 test하기 편함, 시간을 보기가 편함
+		
 		
 
 
